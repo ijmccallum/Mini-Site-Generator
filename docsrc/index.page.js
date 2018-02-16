28b92c4824b84d0d37a42e243c4c9da5dd4d4d42
Iain J McCallum <ijmccallum@hotmail.co.uk>
2018-02-15 23:19:33 -0500

    title

--- docsrc/index.page.js
+++ docsrc/index.page.js
@@ -9,7 +9,7 @@ module.exports = function(){
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>${data.head.title}</title>
+            <title>Mini Site Generator</title>
             <link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
             <link href="main.css" rel="stylesheet">
         </head>
