f45f95e35cdb8f1c12504b83252d05a4e50258dd
Iain J McCallum <ijmccallum@hotmail.co.uk>
2018-02-16 08:05:56 -0500

    npm i to site

--- docsrc/index.page.js
+++ docsrc/index.page.js
@@ -14,31 +14,35 @@ module.exports = function(){
             <link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
             <link href="main.css" rel="stylesheet">
         </head>
-        <body>
+        <body class="text-center">
             <section class="screen-height clr-white bg-black center-section">
                 <h1>Mini Site Generator</h1>
                 <p><em>It's Just <span class="clr-yellow">JavaScript</span></em></p>
             </section>
 
             <section class="screen-height center-section">
-                <p>
-                    <span class="v-center">____.page.js</span>
+                <p class="no-wrap">
+                    <span class="v-center"><span class="hide-small">____</span>.page.js</span>
                     <svg class="svg-icon fill-yellow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 493.356 493.356">
                         <title>is transformed into</title>
                         <path d="M490.498,239.278l-109.632-99.929c-3.046-2.474-6.376-2.95-9.993-1.427c-3.613,1.525-5.427,4.283-5.427,8.282v63.954H9.136   c-2.666,0-4.856,0.855-6.567,2.568C0.859,214.438,0,216.628,0,219.292v54.816c0,2.663,0.855,4.853,2.568,6.563   c1.715,1.712,3.905,2.567,6.567,2.567h356.313v63.953c0,3.812,1.817,6.57,5.428,8.278c3.62,1.529,6.95,0.951,9.996-1.708   l109.632-101.077c1.903-1.902,2.852-4.182,2.852-6.849C493.356,243.367,492.401,241.181,490.498,239.278z"></path>
                     </svg class="v-center">
-                    <span>____.html</span>
+                    <span><span class="hide-small">____</span>.html</span>
                 </p>
             </section>
 
-            <section class="screen-height clr-white bg-blue dotted-bg center-section">
+            <section class="screen-height clr-white bg-blue center-section text-left">
 <pre><code>
 module.exports = function(){
     return html&#96;
-        &lt;p&gt;Just a <span class="clr-yellow">JavaScript</span> template literal!&lt/p&gt
+        &lt;p&gt;It's just <span class="clr-yellow">JavaScript</span>!&lt/p&gt
     &#96;
 }
 </code></pre>
+            </section>
+
+            <section class="screen-height center-section">
+                <code class="inline-code">npm install mini-site-generator</code>
             </section>
         </body>
         </html>
